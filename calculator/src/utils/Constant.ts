76eace32a77ec31f4c4048ce5f011f9f4e29c263
Sumanbassetti <128141423+Sumanbassetti@users.noreply.github.com>
2024-08-04 16:21:07 +0530

    [UPD] Constant file

--- calculator/src/utils/Constant.ts
+++ calculator/src/utils/Constant.ts
@@ -1,5 +1,6 @@
 export class Constants{
     static MultiplicationErrorMsg: string = "Number of columns in the first matrix must be equal to the number of rows in the second matrix."
     static MaximumDimension:number = 100
+    static MinmumDimension:number = 1
     static RowColumnInputErrorMsg: string = `The number of rows and columns should not exceed ${Constants.MaximumDimension}`
 }
